fe05d48fe3a673a4f859547d5614e29f02f385d5
Gourav Jha <gouravjha18011994@gmail.com>
2024-10-11 15:09:53 +0530

    hii this is email api

---
+++ src/main/java/com/scm/Service/EmailService.java
@@ -0,0 +1,23 @@
+package com.scm.Service;
+
+import com.scm.entity.EmailDetails;
+
+//Java Program to Illustrate Creation Of
+//Service Interface
+
+
+
+
+
+//Interface
+public interface EmailService {
+
+	// Method
+	// To send a simple email
+	String sendSimpleMail(EmailDetails details);
+
+	// Method
+	// To send an email with attachment
+	String sendMailWithAttachment(EmailDetails details);
+}
+
